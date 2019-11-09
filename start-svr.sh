030b6961b5514fb4a5648b29bd7ec6af47a4f625
Lucas Gabriel Coliado Bandeira <lucascoliado@hotmail.com>
2019-11-09 06:39:43 -0300

    Clean up

--- start-svr.sh
+++ start-svr.sh
@@ -13,18 +13,18 @@ conf="$(pwd)/adedserv.cfg"
 # Characters
 ralsei="$(pwd)/mods/KC_Mercedes-Benz_Ralsei_V1.wad"
 bonus="/usr/games/SRB2KART/bonuschars.kart"
-chars="${ralsei} ${bonus}"
+allchars="${ralsei} ${bonus}"
 
 # Mods
 combi="$(pwd)/mods/KL_combiring_v3b.lua"
 
 if [ $# -eq 0 ];then
-    tmux new-session -d -s kart_server "srb2kart -dedicated -room 28 -config ${conf} -file ${chars}"
+    tmux new-session -d -s kart_server "srb2kart -dedicated -room 28 -config ${conf} -file ${allchars}"
     print_svr_ok
 elif [ $1 = "combi" ]
 then
     echo "COMBI MODE!!"
-    tmux new-session -d -s kart_server 'srb2kart -dedicated -room 28 -config "${conf}" -file "${chars}" ${combi}"'
+    tmux new-session -d -s kart_server "srb2kart -dedicated -room 28 -config ${conf} -file ${allchars} ${combi}"
     print_svr_ok
 else
     echo "Unknown mode for server. Nothing done."
