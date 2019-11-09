fe0bf072e8b4495fb389cea7586bd36e0ef7dbee
lucasgcb <lucascoliado@hotmail.com>
2019-11-09 07:31:17 +0000

    add handler for unknown gamemodes

--- start-svr.sh
+++ start-svr.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-if [ $# -eq 0 ];then
-    tmux new-session -d -s kart_server 'srb2kart -dedicated -room 28 -config "$(pwd)/adedserv.cfg" -file "/usr/games/SRB2KART/bonuschars.kart"'
-elif [ $1 = "combi" ]
-then
-    echo "COMBI MODE!!"
-    tmux new-session -d -s kart_server 'srb2kart -dedicated -room 28 -config "$(pwd)/adedserv.cfg" -file "/usr/games/SRB2KART/bonuschars.kart" "$(pwd)/mods/KL_combiring_v3b.lua"'
-fi
-
+function print_svr_ok () {
 echo "#########################################################"
 echo "Server initialized with tmux. Session name: kart_server"
 echo "do 'tmux attach -t kart_server' to see logs"
 echo "do 'tmux kill-session -t kart_server' to kill server"
 echo "#########################################################"
+}
+
+if [ $# -eq 0 ];then
+    tmux new-session -d -s kart_server 'srb2kart -dedicated -room 28 -config "$(pwd)/adedserv.cfg" -file "/usr/games/SRB2KART/bonuschars.kart"'
+    print_svr_ok
+elif [ $1 = "combi" ]
+then
+    echo "COMBI MODE!!"
+    tmux new-session -d -s kart_server 'srb2kart -dedicated -room 28 -config "$(pwd)/adedserv.cfg" -file "/usr/games/SRB2KART/bonuschars.kart" "$(pwd)/mods/KL_combiring_v3b.lua"'
+    print_svr_ok
+else
+    echo "Unknown mode for server. Nothing done."
+fi
