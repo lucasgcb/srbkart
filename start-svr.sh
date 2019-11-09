255d5dc7f3706b84e8ece7555539b6ca9dbab5c4
lucasgcb <lucascoliado@hotmail.com>
2019-11-09 08:57:53 +0000

    Add server options

--- start-svr.sh
+++ start-svr.sh
@@ -8,13 +8,23 @@ echo "do 'tmux kill-session -t kart_server' to kill server"
 echo "#########################################################"
 }
 
+conf="$(pwd)/adedserv.cfg"
+
+# Characters
+ralsei="$(pwd)/mods/KC_Mercedes-Benz_Ralsei_V1.wad"
+bonus="/usr/games/SRB2KART/bonuschars.kart"
+chars="${ralsei} ${bonus}"
+
+# Mods
+combi="$(pwd)/mods/KL_combiring_v3b.lua"
+
 if [ $# -eq 0 ];then
-    tmux new-session -d -s kart_server 'srb2kart -dedicated -room 28 -config "$(pwd)/adedserv.cfg" -file "/usr/games/SRB2KART/bonuschars.kart"'
+    tmux new-session -d -s kart_server "srb2kart -dedicated -room 28 -config ${conf} -file ${chars}"
     print_svr_ok
 elif [ $1 = "combi" ]
 then
     echo "COMBI MODE!!"
-    tmux new-session -d -s kart_server 'srb2kart -dedicated -room 28 -config "$(pwd)/adedserv.cfg" -file "/usr/games/SRB2KART/bonuschars.kart" "$(pwd)/mods/KL_combiring_v3b.lua"'
+    tmux new-session -d -s kart_server 'srb2kart -dedicated -room 28 -config "${conf}" -file "${chars}" ${combi}"'
     print_svr_ok
 else
     echo "Unknown mode for server. Nothing done."
