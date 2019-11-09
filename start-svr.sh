4226c6549d4bcde1077b40de0b601a81deb02b82
lucasgcb <lucascoliado@hotmail.com>
2019-11-09 07:21:13 +0000

    Add combi mode option

--- start-svr.sh
+++ start-svr.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-tmux new-session -d -s kart_server 'srb2kart -dedicated -room 28 -config "$(pwd)/adedserv.cfg" -file "/usr/games/SRB2KART/bonuschars.kart"
-'
+
+if [ $# -eq 0 ];then
+    tmux new-session -d -s kart_server 'srb2kart -dedicated -room 28 -config "$(pwd)/adedserv.cfg" -file "/usr/games/SRB2KART/bonuschars.kart"'
+elif [ $1 = "combi" ]
+then
+    echo "COMBI MODE!!"
+    tmux new-session -d -s kart_server 'srb2kart -dedicated -room 28 -config "$(pwd)/adedserv.cfg" -file "/usr/games/SRB2KART/bonuschars.kart" "$(pwd)/mods/KL_combiring_v3b.lua"'
+fi
+
 echo "#########################################################"
-echo "Kart session running with tmux session name  'kart_server'"
+echo "Server initialized with tmux. Session name: kart_server"
 echo "do 'tmux attach -t kart_server' to see logs"
-echo "do 'tmux kill_session kart_server' to kill server"
+echo "do 'tmux kill-session -t kart_server' to kill server"
 echo "#########################################################"
