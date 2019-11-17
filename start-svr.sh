038943be3358f5eb1802b307c4ed92d44a3ff386
Lucas Gabriel Coliado Bandeira <lucascoliado@hotmail.com>
2019-11-16 22:18:59 -0300

    Add maps, combimaps options

--- start-svr.sh
+++ start-svr.sh
@@ -16,7 +16,7 @@ arc="$(pwd)/mods/arc.pk3"
 neco="$(pwd)/mods/necoarc.pk3"
 bonus="/usr/games/SRB2KART/bonuschars.kart"
 allchars="${ralsei} ${bonus} ${neco} ${arc}"
-
+mappack = "${RA}"
 # Mods
 combi="$(pwd)/mods/KL_combiring_v3b.lua"
 
@@ -27,6 +27,16 @@ elif [ $1 = "combi" ]
 then
     echo "COMBI MODE!!"
     tmux new-session -d -s kart_server "srb2kart -dedicated -room 28 -config ${conf} -file ${allchars} ${combi}"
+    print_svr_ok
+elif [ $1 = "maps" ]
+then
+    echo "Thick Maps"
+    tmux new-session -d -s kart_server "srb2kart -dedicated -room 28 -config ${conf} -file ${allchars} ${mappack}"
+    print_svr_ok
+elif [ $1 = "combimaps" ]
+then
+    echo "Thick Combi Maps"
+    tmux new-session -d -s kart_server "srb2kart -dedicated -room 28 -config ${conf} -file ${allchars} ${combi} ${mappack}"
     print_svr_ok
 else
     echo "Unknown mode for server. Nothing done."
